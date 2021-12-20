b21055bb201e60e57164ee10b5cab860f8ebebef
Srishti Jain <60124845+srs721@users.noreply.github.com>
2021-12-20 12:08:19 +0530

    Update __init__.py

--- interfaceMonitoringNotification/__init__.py
+++ interfaceMonitoringNotification/__init__.py
@@ -29,7 +29,38 @@ def send_data_to_serviceBus(data_body):
         with sender:
             send_message(sender)   
 
+def process_data(data_body):
 
+    try:
+        data = json.loads(data_body)
+        items = [];
+
+        #### Fetch company_sysid ####
+        for i in data:
+            company_sysid = i   
+            device_data = data[i]
+        
+        #### Fetch CI_sysid, Manufacturer and Interface details ####  
+        for i in device_data:
+            Ci_sysid = device_data[i]['CI_sysid']
+            Manufacturer = device_data[i]['Manufacturer']
+            Interface = device_data[i]['Interface']
+            Company = device_data[i]['Company']
+            items.append({ 'retryCount': 0, 'configurationItem' : {'sysid' : Ci_sysid, 'company' : {'name': Company, 'sysId' : company_sysid }, 'manufacturer_name' : Manufacturer, 'interfaces': Interface }})
+        
+        #### Pass data string data to service bus ####
+        for i in range(len(items)):
+            data = json.dumps(items[i])
+            data = data.replace('null', '""')
+            logging.info(data)
+            return_data = send_data_to_serviceBus(data)
+        return return_data
+
+    except Exception as e:
+        logging.error(str(e))
+        return str(e)
+    
+    
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('Python HTTP trigger function processed a request.')
